27df3cfe6581eb5c105e3dcf22a6af25bb3f2f67
Niels-1060880 <113016937+Niels-1060880@users.noreply.github.com>
2022-09-28 14:44:49 +0200

    UPDATE: worked on enemy class

---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame
+from enemy
+
+# Screen
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Clock
+clock = pygame.time.Clock()
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+
+class Game():
+    def __init__(self):
+        pass
+
+    def run():
+        pass
+
+
+running = True
+
+while running:
+    screen.fill((BLACK))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            runnnig = False

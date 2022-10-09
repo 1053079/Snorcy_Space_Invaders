5529bda0585952c0062ffb8b4103e25314034b89
1053079 <1053079@hr.nl>
2022-10-09 21:12:12 +0200

    made some small changes and added some comments

--- main.py
+++ main.py
@@ -28,30 +28,6 @@ RED = (255, 0, 0)
 start_img = pygame.image.load('images/start-button2.png').convert_alpha()
 exit_img = pygame.image.load('images/exit_button.png').convert_alpha()
 
-class Bullet(pygame.sprite.Sprite):
-    def __init__(self, player):
-        super().__init__()
-        self.image = pygame.image.load("assets/bullet.png")
-        self.surf = pygame.Surface((32, 32))
-        self.rect = self.surf.get_rect(center=(player.rect.midtop))
-        self.fired = False
-        
-    # this checks for player input
-    def fire(self, player):
-        pressedKeys = pygame.key.get_pressed()
-        if pressedKeys[K_SPACE] and self.fired == False:
-            self.rect = (self.surf.get_rect(center=(player.rect.midtop)))
-            self.fired = True
-         # Speed of the bullets
-        if self.fired == True:
-            screen.blit(self.image, self.rect)
-            self.rect.move_ip(0, -5)
-
-            if (self.rect.top < 1):
-                self.rect.top = 600
-                self.fired = False
-
-
 class Game():
     def __init__(self):
         # Initial enemy spawn (Niels)
@@ -88,36 +64,6 @@ class Game():
         # Loops through the enemy 3 list and renders the enemies (Niels)
         for enemies in self.enemies_3:
             enemies.render()
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load("assets/playership.png")
-        self.surf = pygame.Surface((100, 150))
-        self.rect = self.surf.get_rect(center=(400, 525))
-
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
-
-    def update(self):
-        pressedKeys = pygame.key.get_pressed()
-
-        if self.rect.left > 0:
-            if pressedKeys[K_LEFT]:
-                self.rect.move_ip(-5, 0)
-
-        if self.rect.right < SCREEN_WIDTH:
-            if pressedKeys[K_RIGHT]:
-                self.rect.move_ip(5, 0)
-
-        if self.rect.top > 0:
-            if pressedKeys[K_UP]:
-                self.rect.move_ip(0, -5)
-
-        if self.rect.bottom < SCREEN_HEIGHT:
-            if pressedKeys[K_DOWN]:
-                self.rect.move_ip(0, 5)
-
 
 class Asteroid(pygame.sprite.Sprite):
     def __init__(self):
@@ -159,13 +105,11 @@ class AsteroidXY(pygame.sprite.Sprite):
 start_button = Button(100, 240, start_img, 1)
 exit_button = Button(450, 240, exit_img, 1)
 
-P1 = Player()
+
 A1 = Asteroid()
 A2 = Asteroid()
 A3 = Asteroid()
 AXY1 = AsteroidXY()
-
-bullets = Bullet(P1)
 
 asteroidGroup = pygame.sprite.Group()
 asteroidGroup.add(A1)

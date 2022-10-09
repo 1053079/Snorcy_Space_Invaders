f3b995aca630ae4fc305384ec2f3353a0150335c
Niels-1060880 <113016937+Niels-1060880@users.noreply.github.com>
2022-10-09 15:48:13 +0200

    UPDATE: fixed the playerclass, it got chenged

--- lib/player.py
+++ lib/player.py
@@ -1,31 +1,24 @@
-
 import pygame
 
-# Button class
-class Button():
-    def __init__(self, x, y, image, scale):
-        width = image.get_width()
-        height = image.get_height()
-        self.image = pygame.transform.scale(image, (int(width * scale),int(height * scale)))
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y)
-        self.clicked = False
+# Load Player ship image
+PLAYER_SHIP = pygame.image.load("assets/playership.png"))
 
-    def draw(self, surface):
-        action = False
-        # Get mouse position
-        pos = pygame.mouse.get_pos()
+# Screen
+SCREEN_WIDTH=800
+SCREEN_HEIGHT=600
+screen=pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock=pygame.time.Clock
 
-        # Check mouseover and clicked conditions
-        if self.rect.collidepoint(pos):
-            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
-                self.clicked = True
-                action = True
+# Player Class
 
-        if pygame.mouse.get_pressed()[0] == 0:
-            self.clicked = False
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.ship_img=PLAYER_SHIP
+        self.rect=self.image.get_rect()
+        self.rect.centerx=SCREEN_WIDTH/2
+        self.rect.y=SCREEN_HEIGHT - 10
+        self.mask=pygame.mask.from_surface(self.ship_img)
 
-        # Draw button on screen
-        surface.blit(self.image,(self.rect.x, self.rect.y))
-
-        return action
+    def update(self):
+        pass

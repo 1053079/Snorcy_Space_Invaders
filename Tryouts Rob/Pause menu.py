9d33c935e7fb3be0e48e7c227612ce737a914e3c
Rob <1059422@hr.nl>
2022-10-11 15:45:07 +0200

    update Rob ( Tryout veranderd. niet bijzonder dus)

--- Tryouts Rob/Pause menu.py
+++ Tryouts Rob/Pause menu.py
@@ -12,7 +12,7 @@ pygame.display.set_caption("SNORCY pause Menu")
 
 # game variables
 game_paused = False
-menu_state = "main"
+start_menu_main = "main"
 
 # Define fonts
 font = pygame.font.SysFont("arialblack", 40)
@@ -23,11 +23,14 @@ text_colour = (255, 255, 255)
 # Load button images
 resume_image = pygame.image.load("images/button_resume.png").convert_alpha()
 quit_image = pygame.image.load("images/button_quit.png").convert_alpha()
-
+turtorial_image = pygame.image.load("images/turtorial2.png").convert_alpha()
+back_image = pygame.image.load("images/button_back.png").convert_alpha()
 
 # Create button instances
 resume_button = button.Button(304, 125, resume_image, 1)
 quit_button = button.Button(336, 375, quit_image, 1)
+turtorial_button = button.Button(320, 250, turtorial_image, 1)
+back_button = button.Button(320, 250, back_image, 1)
 
 
 def draw_text(text, font, text_col, x, y):
@@ -44,12 +47,17 @@ while run:
     # Check if game is paused
     if game_paused == True:
         # Check menu state
-        if menu_state == "main":
+        if start_menu_main == "main":
             # Draw pause screen buttons
             if resume_button.draw(screen):
                 game_paused = False
+            if turtorial_button.draw(screen):
+                start_menu_main = "turtorial"
             if quit_button.draw(screen):
                 run = False
+        if start_menu_main == "turtorial":
+            if back_button.draw(screen):
+                start_menu_main = "main"        
     else:
         draw_text("Press ESC to pause", font, text_colour, 180, 250)
 

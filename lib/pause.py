58215cb4e6d7c5c73b7f798f0fea9235f26dcfe1
Rob <1059422@hr.nl>
2022-10-21 15:12:35 +0200

    testing

--- lib/pause.py
+++ lib/pause.py
@@ -1,11 +1,15 @@
 import pygame
 
 class Pause:
-    def __init__(self):
+    def __init__(self,screen,Button):
         self.paused = True
+        resume_image = pygame.image.load("assets/img/button_resume.png").convert_alpha()
+        resume_button = Button(400 - 95.5, 155, resume_image, 1)
 
         # the loop for pausing
         while self.paused:
+            if resume_button.draw(screen):
+                start_menu = True
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -14,3 +18,4 @@ class Pause:
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         self.paused = False
+            pygame.display.update()
